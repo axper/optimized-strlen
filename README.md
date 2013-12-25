bb9f29684800cab88dca6930adf2733494cd4a19
Axper Jan <483ken@gmail.com>
2013-12-25 20:40:08 +0400

    initial commit, added optimized-strlen.c, readme, cmake build system

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-optimized-strlen
+Optimized `strlen`: shows whether a double word value has one or more of its bytes set to 0
 ================
 
-This optimized/obfuscated code is able to figure out whether a double word value has one or more of its bytes set to 0
+Building on Linux
+-----------------
+This project uses Cmake build system.
+On Linux, simply type:
+
+	cmake .
+	make
+
+The resulting executable will be called `caller`
+
+Building on Windows
+-------------------
+See http://www.cmake.org/cmake/help/runningcmake.html
+
---
+++ optimized-strlen.c
@@ -0,0 +1,74 @@
+#include <stdint.h> // uint32_t
+#include <stdio.h> // printf
+
+// Assumes little endian
+void printBits(size_t const size, void const * const ptr)
+{
+	unsigned char *b = (unsigned char*) ptr;
+	unsigned char byte;
+	int i, j;
+
+	for (i = size - 1; i >= 0; i--) {
+		for (j = 7; j >= 0; j--) {
+			byte = b[i] & (1 << j);
+			byte >>= j;
+			printf("%u", byte);
+		}
+	}
+}
+
+int main()
+{
+	uint32_t a = 0;
+	uint32_t d = 0;
+	const uint32_t magic = 0x7EFEFEFF;
+	const uint32_t magicRev = magic ^ 0xFFFFFFFF;
+
+	const uint32_t numbers[] = {
+		0x00000000, 0x00000001, 0x34003231,
+		0x34323331, 0x01010101
+	};
+
+
+	for (int i = 0; i != sizeof(numbers) / sizeof(numbers[ 0 ]); i++) {
+		a = numbers[ i ];
+		d = magic;
+
+		printf("a:            ");
+		printBits(sizeof(a), &a);
+		printf("\n");
+
+		d = a + d;
+
+		printf("a+magic:      ");
+		printBits(sizeof(d), &d);
+		printf("\n");
+
+		a = a ^ 0xFFFFFFFF;
+
+		printf("!a:           ");
+		printBits(sizeof(a), &a);
+		printf("\n");
+
+		a = a ^ d;
+
+		printf("result:       ");
+		printBits(sizeof(a), &a);
+		printf("\n");
+
+		a = a & magicRev;
+
+		printf("              ");
+		printBits(sizeof(a), &a);
+
+		if (a == 0) {
+			printf(" (zero)\n");
+		} else {
+			printf(" (at least one)\n");
+		}
+
+		printf("\n");
+	}
+
+	return 0;
+}
